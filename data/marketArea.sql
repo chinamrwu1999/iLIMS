bf792b3862a7e06372d4c7efe68cf0e5ba290e3d
吴志诚 <wuzhicheng@fudan.edu.cn>
2024-06-15 17:03:16 +0800

    sdfsjkf

---
+++ data/marketArea.sql
@@ -0,0 +1,14 @@
+insert into geo (`geoId`,`geoName`,`geoType`) VALUES
+('chinaLand','国内市场','SALES_TERRITORY'),
+('chinaEast','东大区','SALES_TERRITORY'),
+('chinaSouth','南大区','SALES_TERRITORY'),
+('chinaNorth','北大区','SALES_TERRITORY'),
+('HJL','黑吉辽','SALES_TERRITORY'),
+('HBTJ','河北天津','SALES_TERRITORY'),
+('SGNQ','陕甘宁青','SALES_TERRITORY'),
+('CQXZ','重庆-西藏','SALES_TERRITORY'),
+('SXNM','山西|内蒙','SALES_TERRITORY'),
+('JXFJ','江西|福建','SALES_TERRITORY'),
+('YNGZ','云南|贵州','SALES_TERRITORY'),
+('YNQ','广东广西海南','SALES_TERRITORY'),
+
